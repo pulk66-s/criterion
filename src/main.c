59eda7efd713e8f0b9b2e678637625daa2523229
pulk66-s <hugo.marrasse@epitech.eu>
2023-01-30 14:48:05 +0100

    [ADD] Add of config file argument

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include "launcher.h"
 #include "types/files.h"
+#include "config.h"
 #include <stdio.h>
 
-int main(void)
+static void config_criterion(int ac, char **av)
 {
+    if (ac < 2)
+        return;
+    set_test_folder(av[1]);
+}
+
+int main(int ac, char **av)
+{
+    config_criterion(ac, av);
     launch_tests();
     return 0;
 }
