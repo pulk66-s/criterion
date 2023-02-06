b8ead0dd8ac353e3326beda784031370150e5a5a
pulk66-s <hugo.marrasse@epitech.eu>
2023-02-07 00:17:00 +0100

    [FIX] Fix of bad practice code
    Fix of bad practice code in io.c:
      Make all global static
      Creation of a var to the io redirection

--- src/lib/asserts/io.c
+++ src/lib/asserts/io.c
@@ -5,21 +5,22 @@
 #include "lib/asserts/io.h"
 #include "lib/tests/tests.h"
 
-int stdout_save;
-char std_output_buffer[BUFFER_SIZE] = {0};
-char std_error_buffer[BUFFER_SIZE] = {0};
+static int stdout_save;
+static char std_output_buffer[BUFFER_SIZE] = {0};
+static char std_error_buffer[BUFFER_SIZE] = {0};
+static char output_name[] = "tmp/NUL";
 
 void replace_stdout()
 {
     fflush(stdout); //clean everything first
     stdout_save = dup(STDOUT_FILENO); //save the stdout state
-    freopen("NUL", "a", stdout); //redirect stdout to null pointer
+    freopen(output_name, "a", stdout); //redirect stdout to null pointer
     setvbuf(stdout, std_output_buffer, _IOFBF, BUFFER_SIZE); //set buffer to stdout
 }
 
 void restore_stdout()
 {
-    freopen("NUL", "a", stdout); //redirect stdout to null again
+    freopen(output_name, "a", stdout); //redirect stdout to null again
     dup2(stdout_save, STDOUT_FILENO); //restore the previous state of stdout
     setvbuf(stdout, NULL, _IONBF, BUFFER_SIZE); //disable buffer to print to screen instantly
 }
@@ -48,13 +49,13 @@ void replace_stderr()
 {
     fflush(stderr); //clean everything first
     stdout_save = dup(STDERR_FILENO); //save the stderr state
-    freopen("NUL", "a", stderr); //redirect stderr to null pointer
+    freopen(output_name, "a", stderr); //redirect stderr to null pointer
     setvbuf(stderr, std_error_buffer, _IOFBF, BUFFER_SIZE); //set buffer to stderr
 }
 
 void restore_stderr()
 {
-    freopen("NUL", "a", stderr); //redirect stderr to null again
+    freopen(output_name, "a", stderr); //redirect stderr to null again
     dup2(stdout_save, STDERR_FILENO); //restore the previous state of stderr
     setvbuf(stderr, NULL, _IONBF, BUFFER_SIZE); //disable buffer to print to screen instantly
 }
